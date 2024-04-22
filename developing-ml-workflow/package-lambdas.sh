c93e6e570c45fdd677c2e17cb27939b5d653158b
Stefano Emilio Campanini <stefano.campanini@musement.com>
2024-04-22 18:08:30 +0200

    wip

---
+++ developing-ml-workflow/package-lambdas.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Package the Lambda with dependecies
+echo Package the serializeImageData Lambda with dependecies   
+cd lambdas/serializeImageData
+rm -rf package
+mkdir package
+cp lambda_function.py package/
+pip install --target ./package boto3
+cd package
+rm -rf ../*.zip
+zip -r ../lambda_serializeImageData_package.zip .
+
+cd ../../..
+
+echo Package the classifyEncodedImg Lambda with dependecies   
+cd lambdas/classifyEncodedImg
+rm -rf package
+mkdir package
+cp lambda_function.py package/
+pip install sagemaker --target ./package
+cd package
+## Reduce the size
+find . -type d -name "tests" -exec rm -rfv {} +
+find . -type d -name "__pycache__" -exec rm -rfv {} +
+rm -rf ../*.zip
+zip -r ../lambda_classifyEncodedImg_package.zip .
+
+cd ../../..
--- developing-ml-workflow/packge-lambdas.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Package the Lambda with dependecies
-echo Package the Lambdas with dependecies   
-cd lambdas/serializeImageData
-mkdir package
-pip install --target ./package boto3
-cd package
-zip -r ../lambda_serializeImageData_package.zip .
