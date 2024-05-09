158e665a3619e415bc41e08fea99946e7ac35a5b
Campa <stefano.campanini@gmail.com>
2024-05-09 17:13:04 +0000

    wip

--- nlp-image-classification/hpo.py
+++ nlp-image-classification/hpo.py
@@ -19,12 +19,28 @@ def test(model, test_loader):
     pass
 
 def train(model, train_loader, criterion, optimizer):
+
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
     
 def net():
     '''
@@ -74,6 +90,46 @@ if __name__=='__main__':
     TODO: Specify all the hyperparameters you need to use to train your model.
     '''
     
-    args=parser.parse_args()
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=2,
+        metavar="N",
+        help="number of epochs to train (default: 14)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
+    )
+    args = parser.parse_args()
     
     main(args)
+
+    train_kwargs = {"batch_size": args.batch_size}
+    test_kwargs = {"batch_size": args.test_batch_size}
+
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+    model = Net()
+
+    optimizer = optim.Adadelta(model.parameters(), lr=args.lr)
+
+    for epoch in range(1, args.epochs + 1):
+        train(model, train_loader, optimizer, epoch)
+        test(model, test_loader)
+    
+    torch.save(model.state_dict(), "nlp-img-classification-train-deploy.pt")
